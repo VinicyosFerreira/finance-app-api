61894f3ada4b05a9a7de1cb5675464c4248d42f0
Vinicyos Ferreira <vinifr987@gmail.com>
2025-10-03 23:16:29 -0300

    feat: add CreateUserController and validations

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 import 'dotenv/config';
 import express from 'express';
 import process from 'process';
+import { CreateUserController } from './src/controllers/create-user.js';
 
 const app = express();
 app.use(express.json());
 const port = process.env.PORT;
+
+app.post('/api/users', async (req, res) => {
+  const createdUserController = new CreateUserController();
+
+  const { statusCode, body } = await createdUserController.execute(req);
+
+  res.status(statusCode).json(body);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
---
+++ src/controllers/create-user.js
@@ -0,0 +1,43 @@
+import { CreateUserUseCase } from '../use-cases/create-user.js';
+import validator from 'validator';
+import { badRequest, created, serverError } from './helper.js';
+
+export class CreateUserController {
+  async execute(httpRequest) {
+    try {
+      const params = httpRequest.body;
+
+      // validar a requisição(campos obrigatórios, tamanho de senha e e-mail)
+      const requiredFields = ['first_name', 'last_name', 'email', 'password'];
+      for (const field of requiredFields) {
+        if (!params[field] || params[field].trim().length === 0) {
+          return badRequest({ message: `Missing field: ${field}` });
+        }
+      }
+
+      // validação de senha
+      const passwordIsValid = params.password.length < 6;
+      if (passwordIsValid) {
+        return badRequest({
+          message: 'Password must be at least 6 characters',
+        });
+      }
+
+      // validação de email
+      const isEmailValid = validator.isEmail(params.email);
+      if (!isEmailValid) {
+        return badRequest({ message: 'Invalid email' });
+      }
+
+      // chamar o useCase
+      const createdUserUseCase = new CreateUserUseCase();
+      const createdUser = await createdUserUseCase.execute(params);
+
+      // retornar o resultado para client(usuário)
+      return created(createdUser);
+    } catch (error) {
+      console.log(error);
+      return serverError();
+    }
+  }
+}
