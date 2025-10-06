e6799ce55736156bcd78f173e8e2de85c2e09ec6
Vinicyos Ferreira <vinifr987@gmail.com>
2025-10-06 10:32:18 -0300

    feat: add GetUserByIdUseCase

---
+++ src/use-cases/get-user-by-id.js
@@ -0,0 +1,9 @@
+import { PostgresGetUserByIdRepository } from '../repositories/postgres/get-user-by-id';
+
+export class GetUserByIdUseCase {
+  async execute(userId) {
+    const postgresGetUserByIdRepository = new PostgresGetUserByIdRepository();
+    const user = await postgresGetUserByIdRepository.execute(userId);
+    return user;
+  }
+}
