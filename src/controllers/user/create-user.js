023ea0508d34912bd2557d5683d4bdee52913aa1
Vinicyos Ferreira <vinifr987@gmail.com>
2025-10-29 19:46:20 -0300

    feat: add zod to CreateUserController

--- src/controllers/user/create-user.js
+++ src/controllers/user/create-user.js
@@ -1,44 +1,19 @@
 import { EmailAlreadyInUseError } from '../../errors/user.js';
-import {
-  invalidPasswordResponse,
-  invalidEmailResponse,
-  checkIfPasswordIsValid,
-  checkIfEmailIsValid,
-  badRequest,
-  created,
-  serverError,
-  validateRequireFields,
-  requiredFieldsIsMissingResponse,
-} from '../helpers/index.js';
+import { createUserSchema } from '../../schemas/index.js';
+import { badRequest, created, serverError } from '../helpers/index.js';
+import { ZodError } from 'zod';
 
 export class CreateUserController {
   constructor(createUserUseCase) {
     this.createUserUseCase = createUserUseCase;
   }
+
   async execute(httpRequest) {
     try {
       const params = httpRequest.body;
 
-      // validar a requisição(campos obrigatórios, senha e e-mail)
-      const requiredFields = ['first_name', 'last_name', 'email', 'password'];
-      const { missingField, ok: requiredFieldsWereProvided } =
-        validateRequireFields(params, requiredFields);
-
-      if (!requiredFieldsWereProvided) {
-        return requiredFieldsIsMissingResponse(missingField);
-      }
-
-      // validação de senha
-      const passwordIsValid = checkIfPasswordIsValid(params.password);
-      if (!passwordIsValid) {
-        return invalidPasswordResponse();
-      }
-
-      // validação de email
-      const isEmailValid = checkIfEmailIsValid(params.email);
-      if (!isEmailValid) {
-        return invalidEmailResponse();
-      }
+      // validar a requisição com schemas e utilizando o Zod
+      await createUserSchema.parseAsync(params);
 
       // chamar o useCase
       const createdUser = await this.createUserUseCase.execute(params);
@@ -47,6 +22,10 @@ export class CreateUserController {
       return created(createdUser);
     } catch (error) {
       console.log(error);
+
+      if (error instanceof ZodError) {
+        return badRequest({ message: error.issues[0].message });
+      }
 
       if (error instanceof EmailAlreadyInUseError) {
         return badRequest({ message: error.message });
