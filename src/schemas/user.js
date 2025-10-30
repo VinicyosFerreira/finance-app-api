750f2449de796865db4ed8958c812ef20e42f885
Vinicyos Ferreira <vinifr987@gmail.com>
2025-10-30 10:57:12 -0300

    refactor: remove deprecated property in the Zod schema

--- src/schemas/user.js
+++ src/schemas/user.js
@@ -3,35 +3,35 @@ import { z } from 'zod';
 export const createUserSchema = z.object({
   first_name: z
     .string({
-      message: 'First name is required.',
+      error: 'First name is required.',
     })
     .trim()
     .min(1, {
-      message: 'First name is required.',
+      error: 'First name is required.',
     }),
   last_name: z
     .string({
-      message: 'Last name is required.',
+      error: 'Last name is required.',
     })
     .trim()
     .min(1, {
-      message: 'Last name is required.',
+      error: 'Last name is required.',
     }),
   email: z
     .email({
-      message: 'Plase provide a valid e-mail.',
+      error: 'Plase provide a valid e-mail.',
     })
     .trim()
     .min(1, {
-      message: 'E-mail is required.',
+      error: 'E-mail is required.',
     }),
   password: z
     .string({
-      message: 'Password is required.',
+      error: 'Password is required.',
     })
     .trim()
     .min(6, {
-      message: 'Password must be at least 6 characters.',
+      error: 'Password must be at least 6 characters.',
     }),
 });
 
