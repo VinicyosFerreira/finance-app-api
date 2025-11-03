37b99bf0e6fbc07718d203ed9581ff3f06c5ba2c
Vinicyos Ferreira <vinifr987@gmail.com>
2025-11-03 11:44:29 -0300

    feat: ensure getUserById, UpdateUser e DeleteUserRepository use Prisma

--- src/repositories/postgres/user/update-user.js
+++ src/repositories/postgres/user/update-user.js
@@ -1,34 +1,12 @@
-import { PostgresHelper } from '../../../db/postgres/helper.js';
+import { prisma } from '../../../../prisma/prisma.js';
 
 export class PostgresUpdateUserRepository {
   async execute(userId, updateUserParams) {
-    // Criar 2 variáveis para armazenar a query
-    const updateFields = [];
-    const updateValues = [];
-
-    // Percorrer o objeto updateUserParams e montar a query
-    // Object keys - percorre o objeto e retorna as chaves num array
-    // forEach - percorre cada chave apenas(key), não pecorre os values e objeto inteiro
-    Object.keys(updateUserParams).forEach((key) => {
-      updateFields.push(`${key} = $${updateFields.length + 1}`);
-      updateValues.push(updateUserParams[key]);
+    return await prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: updateUserParams,
     });
-
-    // Adicionar o ID do usuário na query
-    updateValues.push(userId);
-
-    // Realizar a query de atualização de forma dinâmica
-    const updateQuery = `
-        UPDATE users
-        SET ${updateFields.join(', ')}
-        WHERE ID = $${updateValues.length}
-        RETURNING *
-    `;
-
-    // Executar a query
-    const updateUser = await PostgresHelper.query(updateQuery, updateValues);
-
-    // Retornar o usuário atualizado
-    return updateUser[0];
   }
 }
