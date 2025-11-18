57b7af71e19954a7e0c6450cd8f5f03e4fedf121
Vinicyos Ferreira <vinifr987@gmail.com>
2025-11-17 22:58:53 -0300

    test: ensure thorough tests in GetUserBalanceController

---
+++ src/controllers/user/get-user-balance.test.js
@@ -0,0 +1,66 @@
+import { faker } from '@faker-js/faker';
+import { GetUserBalanceController } from './get-user-balance.js';
+
+describe('Get User Balance Controller', () => {
+  // stub
+  class GetUserBalanceUseCaseStub {
+    execute() {
+      return faker.number.int();
+    }
+  }
+
+  const makeSut = () => {
+    const getUserBalanceUseCaseStub = new GetUserBalanceUseCaseStub();
+    const sut = new GetUserBalanceController(getUserBalanceUseCaseStub);
+    return { sut, getUserBalanceUseCaseStub };
+  };
+
+  it('should return 200 if getting balance is successful', async () => {
+    // arrange
+    const { sut } = makeSut();
+
+    // result
+    const result = await sut.execute({
+      params: {
+        userId: faker.string.uuid(),
+      },
+    });
+
+    // assert
+    expect(result.statusCode).toBe(200);
+  });
+
+  it('should return 400 if id is not valid', async () => {
+    // arrang
+    const { sut } = makeSut();
+
+    // result
+    const result = await sut.execute({
+      params: {
+        userId: 'invalid_id',
+      },
+    });
+
+    // asset
+    expect(result.statusCode).toBe(400);
+  });
+
+  it('should return 500 if GetUserBalanceUseCase throws', async () => {
+    // arrange
+    const { sut, getUserBalanceUseCaseStub } = makeSut();
+
+    jest
+      .spyOn(getUserBalanceUseCaseStub, 'execute')
+      .mockRejectedValueOnce(new Error());
+
+    // act
+    const result = await sut.execute({
+      params: {
+        userId: faker.string.uuid(),
+      },
+    });
+
+    // assert
+    expect(result.statusCode).toBe(500);
+  });
+});
