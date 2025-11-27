1c4c6211e45e6557a52df31896089c6b3cf100cd
Vinicyos Ferreira <vinifr987@gmail.com>
2025-11-27 11:26:22 -0300

    test: ensure DeleteUserController calls DeleteUserUseCase with correct params

--- src/controllers/user/delete-user.test.js
+++ src/controllers/user/delete-user.test.js
@@ -86,4 +86,16 @@ describe('Delete User Controller', () => {
     // assert
     expect(result.statusCode).toBe(500);
   });
+
+  it('should call DeleteUserUseCase with correct params', async () => {
+    // arrange
+    const { sut, deleteUserUseCaseStub } = makeSut();
+    const executeSpy = jest.spyOn(deleteUserUseCaseStub, 'execute');
+
+    // act
+    await sut.execute(httpRequest);
+
+    // assert
+    expect(executeSpy).toHaveBeenCalledWith(httpRequest.params.userId);
+  });
 });
