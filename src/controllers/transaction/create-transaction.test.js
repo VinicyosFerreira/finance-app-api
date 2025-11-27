ec114846f2c593605cd0e301ea155483bb184d38
Vinicyos Ferreira <vinifr987@gmail.com>
2025-11-27 11:18:48 -0300

    test: ensure CreateTransactionController calls CreateTransactionUseCase with correct params

--- src/controllers/transaction/create-transaction.test.js
+++ src/controllers/transaction/create-transaction.test.js
@@ -213,4 +213,16 @@ describe('Create Transaction Controller', () => {
     // assert
     expect(result.statusCode).toBe(500);
   });
+
+  it('should call CreateTransactionUseCase with correct params', async () => {
+    // arrange
+    const { sut, createTransactionUseCaseStub } = makeSut();
+    const executeSpy = jest.spyOn(createTransactionUseCaseStub, 'execute');
+
+    // act
+    await sut.execute(httpRequest);
+
+    // assert
+    expect(executeSpy).toHaveBeenCalledWith(httpRequest.body);
+  });
 });
