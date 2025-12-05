8f55eb416c1a3096f6d2776eda1054af4ea9f552
Vinicyos Ferreira <vinifr987@gmail.com>
2025-12-05 15:10:24 -0300

    refactor: remove unused code

--- src/controllers/helpers/user.js
+++ src/controllers/helpers/user.js
@@ -1,24 +1,5 @@
-import { badRequest, notFound } from './http.js';
-import validator from 'validator';
-
-export const invalidPasswordResponse = () => {
-  return badRequest({
-    message: 'Password must be at least 6 characters',
-  });
-};
-
-export const invalidEmailResponse = () => {
-  return badRequest({ message: 'Invalid email' });
-};
+import { notFound } from './http.js';
 
 export const userNotFoundResponse = () => {
   return notFound({ message: 'User not found' });
-};
-
-export const checkIfPasswordIsValid = (password) => {
-  return password.length >= 6;
-};
-
-export const checkIfEmailIsValid = (email) => {
-  return validator.isEmail(email);
 };
--- src/controllers/helpers/validation.js
+++ src/controllers/helpers/validation.js
@@ -12,26 +12,3 @@ export const invalidIdResponse = () => {
 export const requiredFieldsIsMissingResponse = (field) => {
   return badRequest({ message: `Missing field: ${field}` });
 };
-
-export const checkIfIsString = (value) => typeof value === 'string';
-
-export const validateRequireFields = (params, requiredFields) => {
-  for (const field of requiredFields) {
-    const fieldIsMissing = !params[field];
-    const fieldIsEmpty =
-      checkIfIsString(params[field]) &&
-      validator.isEmpty(params[field], { ignore_whitespace: true });
-
-    if (fieldIsMissing || fieldIsEmpty) {
-      return {
-        missingField: field,
-        ok: false,
-      };
-    }
-  }
-
-  return {
-    missingField: undefined,
-    ok: true,
-  };
-};
