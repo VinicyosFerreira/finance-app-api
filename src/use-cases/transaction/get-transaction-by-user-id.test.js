0ffcbd95a706671539896b806bca64270e6e5ff8
Vinicyos Ferreira <vinifr987@gmail.com>
2025-12-11 12:07:07 -0300

    test: ensure thorough tests in GetTransactionByUserIdUseCase

---
+++ src/use-cases/transaction/get-transaction-by-user-id.test.js
@@ -0,0 +1,123 @@
+import { faker } from '@faker-js/faker';
+import { GetTransactionUserByIdUseCase } from './get-transaction-by-user-id';
+import { UserNotFoundError } from '../../errors/user';
+
+describe('Get Transaction By User ID Use Case', () => {
+  // stub
+  class GetTransactionByUserIdRepository {
+    async execute() {
+      return [];
+    }
+  }
+
+  class GetUserByIdRepository {
+    async execute(userId) {
+      return {
+        id: userId,
+        first_name: faker.person.firstName(),
+        last_name: faker.person.lastName(),
+        email: faker.internet.email(),
+        password: faker.internet.password({
+          length: 7,
+        }),
+      };
+    }
+  }
+
+  const makeSut = () => {
+    const getTransactionByUserIdRepository =
+      new GetTransactionByUserIdRepository();
+    const getUserByIdRepository = new GetUserByIdRepository();
+
+    const sut = new GetTransactionUserByIdUseCase(
+      getTransactionByUserIdRepository,
+      getUserByIdRepository
+    );
+
+    return {
+      sut,
+      getTransactionByUserIdRepository,
+      getUserByIdRepository,
+    };
+  };
+
+  it('should return transaction successfully by user ID', async () => {
+    // arrange
+    const { sut } = makeSut();
+    // act
+    const result = await sut.execute(faker.string.uuid());
+
+    // assert
+    expect(result).toEqual([]);
+  });
+
+  it('should throw UserNotFoundError if user does not exist', async () => {
+    // arrange
+    const { sut, getUserByIdRepository } = makeSut();
+    const userId = faker.string.uuid();
+    jest.spyOn(getUserByIdRepository, 'execute').mockReturnValueOnce(null);
+
+    // act
+    const promise = sut.execute(userId);
+
+    // assert
+    await expect(promise).rejects.toThrow(new UserNotFoundError(userId));
+  });
+
+  it('should call GetUserByIdRepository with correct user ID', async () => {
+    // arrange
+    const { sut, getUserByIdRepository } = makeSut();
+    const userId = faker.string.uuid();
+    const getUserByIdSpy = jest.spyOn(getUserByIdRepository, 'execute');
+
+    // act
+    await sut.execute(userId);
+
+    // assert
+    expect(getUserByIdSpy).toHaveBeenCalledWith(userId);
+  });
+
+  it('should call GetTransactionByUserIdRepository with correct user ID', async () => {
+    // arrange
+    const { sut, getTransactionByUserIdRepository } = makeSut();
+    const userId = faker.string.uuid();
+    const getTransactionByUserIdSpy = jest.spyOn(
+      getTransactionByUserIdRepository,
+      'execute'
+    );
+    // act
+    await sut.execute(userId);
+    // assert
+    expect(getTransactionByUserIdSpy).toHaveBeenCalledWith(userId);
+  });
+
+  it('should throw if GetUserByIdRepository throws', async () => {
+    // arrange
+    const { sut, getUserByIdRepository } = makeSut();
+    const userId = faker.string.uuid();
+    jest
+      .spyOn(getUserByIdRepository, 'execute')
+      .mockRejectedValueOnce(new Error());
+
+    // act
+    const promise = sut.execute(userId);
+
+    // assert
+    await expect(promise).rejects.toThrow();
+  });
+
+  it('should throw if GetTransactionByUserIdRepository throws', async () => {
+    // arrange
+    const { sut, getTransactionByUserIdRepository } = makeSut();
+    const userId = faker.string.uuid();
+    jest
+      .spyOn(getTransactionByUserIdRepository, 'execute')
+      .mockRejectedValueOnce(new Error());
+
+    // act
+    const promise = sut.execute(userId);
+
+    // assert
+    await expect(promise).rejects.toThrow();
+  });
+});
