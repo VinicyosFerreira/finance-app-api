d7e4fd5a9ab04a5fbfdc533457bd58e1666f3346
Vinicyos Ferreira <vinifr987@gmail.com>
2025-12-20 13:13:19 -0300

    test: ensure thorough tests in PostgresCreateUserRepository

--- src/repositories/postgres/user/create-user.test.js
+++ src/repositories/postgres/user/create-user.test.js
@@ -1,10 +1,26 @@
 import { PostgresCreateUserRepository } from './create-user.js';
 import { user } from '../../../tests/index.js';
+import { prisma } from '../../../../prisma/prisma.js';
 
 describe('Create User Repository', () => {
   it('should create a new user on database', async () => {
     const sut = new PostgresCreateUserRepository();
     const result = await sut.execute(user);
-    expect(result).not.toBeNull();
+    expect(result.id).toBe(user.id);
+    expect(result.first_name).toBe(user.first_name);
+    expect(result.last_name).toBe(user.last_name);
+    expect(result.email).toBe(user.email);
+    expect(result.password).toBe(user.password);
+  });
+
+  it('should call Prisma with correct params', async () => {
+    const sut = new PostgresCreateUserRepository();
+    const prismaSpy = jest.spyOn(prisma.user, 'create');
+
+    await sut.execute(user);
+
+    expect(prismaSpy).toHaveBeenCalledWith({
+      data: user,
+    });
   });
 });
