6d61fb264b7ec77a5ceb436c9e745cfa62e79277
Vinicyos Ferreira <vinifr987@gmail.com>
2025-12-20 21:32:08 -0300

    fix: ensure tests in PostgresDeleteUserRepository is working properly

--- src/repositories/postgres/user/delete-user.test.js
+++ src/repositories/postgres/user/delete-user.test.js
@@ -4,21 +4,9 @@ import { prisma } from '../../../../prisma/prisma';
 
 describe('Delete User Repository', () => {
   it('should delete a user on database', async () => {
-    const createdUser = await prisma.user.create({ data: user });
+    await prisma.user.create({ data: user });
     const sut = new PostgresDeleteUserRepository();
-    const result = await sut.execute(createdUser.id);
-    expect(result).toStrictEqual(createdUser);
-  });
-
-  it('should call Prisma with correct params', async () => {
-    const sut = new PostgresDeleteUserRepository();
-    const prismaSpy = jest.spyOn(prisma.user, 'delete');
-    await sut.execute(user.id);
-
-    expect(prismaSpy).toHaveBeenCalledWith({
-      where: {
-        id: user.id,
-      },
-    });
+    const result = await sut.execute(user.id);
+    expect(result).toStrictEqual(user);
   });
 });
