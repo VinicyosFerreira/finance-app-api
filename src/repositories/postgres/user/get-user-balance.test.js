f54e399fa72f63c5af6c5d876e487630417d7a49
Vinicyos Ferreira <vinifr987@gmail.com>
2025-12-26 11:47:50 -0300

    test: ensure PostgresGetUserBalanceRepository calls Prisma with correct values

--- src/repositories/postgres/user/get-user-balance.test.js
+++ src/repositories/postgres/user/get-user-balance.test.js
@@ -2,6 +2,7 @@ import { prisma } from '../../../../prisma/prisma';
 import { user as fakeUser } from '../../../tests/index.js';
 import { faker } from '@faker-js/faker';
 import { PostgresGetUserBalanceRepository } from './get-user-balance.js';
+import { TransactionType } from '../../../generated/prisma/client.js';
 
 describe('Get User Balance Repository', () => {
   it('should get user balance on db', async () => {
@@ -62,5 +63,43 @@ describe('Get User Balance Repository', () => {
     expect(result.expenses.toString()).toBe('2000');
     expect(result.investments.toString()).toBe('6000');
     expect(result.balance.toString()).toBe('2000');
+  });
+
+  it('should call Prisma with correct values', async () => {
+    await prisma.user.create({ data: fakeUser });
+    const prismaSpy = jest.spyOn(prisma.transaction, 'aggregate');
+
+    const sut = new PostgresGetUserBalanceRepository();
+
+    await sut.execute(fakeUser.id);
+
+    expect(prismaSpy).toHaveBeenCalledTimes(3);
+    expect(prismaSpy).toHaveBeenCalledWith({
+      where: {
+        user_id: fakeUser.id,
+        type: TransactionType.EARNING,
+      },
+      _sum: {
+        amount: true,
+      },
+    });
+    expect(prismaSpy).toHaveBeenCalledWith({
+      where: {
+        user_id: fakeUser.id,
+        type: TransactionType.EXPENSE,
+      },
+      _sum: {
+        amount: true,
+      },
+    });
+    expect(prismaSpy).toHaveBeenCalledWith({
+      where: {
+        user_id: fakeUser.id,
+        type: TransactionType.INVESTMENT,
+      },
+      _sum: {
+        amount: true,
+      },
+    });
   });
 });
