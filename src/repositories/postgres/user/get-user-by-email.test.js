e608daaf5ff4b6450b0c3611163205bf6de03a19
Vinicyos Ferreira <vinifr987@gmail.com>
2025-12-26 12:00:03 -0300

    test: ensure thorough tests in PostgresGetUserByEmailRepository

---
+++ src/repositories/postgres/user/get-user-by-email.test.js
@@ -0,0 +1,27 @@
+import { prisma } from '../../../../prisma/prisma';
+import { user as FakeUser } from '../../../tests/index.js';
+import { PostgresGetUserByEmailRepository } from './get-user-by-email.js';
+
+describe('Get User By Email Repository', () => {
+  it('should get user by email on db', async () => {
+    const user = await prisma.user.create({ data: FakeUser });
+    const sut = new PostgresGetUserByEmailRepository();
+
+    const result = await sut.execute(user.email);
+
+    expect(result).toStrictEqual(user);
+  });
+
+  it('should call prisma with correct values', async () => {
+    const sut = new PostgresGetUserByEmailRepository();
+    const prismaSpy = jest.spyOn(prisma.user, 'findUnique');
+
+    await sut.execute(FakeUser.email);
+
+    expect(prismaSpy).toHaveBeenCalledWith({
+      where: {
+        email: FakeUser.email,
+      },
+    });
+  });
+});
