d26033649f34a0fd7fe4bf8a421efd0df695ace3
Vinicyos Ferreira <vinifr987@gmail.com>
2025-12-26 13:30:44 -0300

    test: ensure thorough tests in PostgresUpdateUserRepository

---
+++ src/repositories/postgres/user/update-user.test.js
@@ -0,0 +1,37 @@
+import { prisma } from '../../../../prisma/prisma';
+import { user as fakeUser } from '../../../tests';
+import { faker } from '@faker-js/faker';
+import { PostgresUpdateUserRepository } from './update-user.js';
+
+describe('Update User Repository', () => {
+  const updateUserParams = {
+    id: faker.string.uuid(),
+    first_name: faker.person.firstName(),
+    last_name: faker.person.lastName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+  };
+  it('should update user on db', async () => {
+    const user = await prisma.user.create({ data: fakeUser });
+    const sut = new PostgresUpdateUserRepository();
+
+    const result = await sut.execute(user.id, updateUserParams);
+
+    expect(result).toStrictEqual(updateUserParams);
+  });
+
+  it('should call prisma with correct values', async () => {
+    await prisma.user.create({ data: fakeUser });
+    const sut = new PostgresUpdateUserRepository();
+    const prismaSpy = jest.spyOn(prisma.user, 'update');
+
+    await sut.execute(fakeUser.id, updateUserParams);
+
+    expect(prismaSpy).toHaveBeenCalledWith({
+      where: {
+        id: fakeUser.id,
+      },
+      data: updateUserParams,
+    });
+  });
+});
