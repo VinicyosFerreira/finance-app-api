04051370db3a3dfaa508fde4aca7f384e2565241
Vinicyos Ferreira <vinifr987@gmail.com>
2025-12-26 23:21:35 -0300

    test: ensure thorough test in PostgresDeleteTransactionRepository

---
+++ src/repositories/postgres/transaction/delete-transaction.test.js
@@ -0,0 +1,41 @@
+import { PostgresDeleteTransactionRepository } from './delete-transaction.js';
+import { user as fakeUser, transaction } from '../../../tests/index.js';
+import { prisma } from '../../../../prisma/prisma.js';
+import dayjs from 'dayjs';
+
+describe('Delete Transaction Repository', () => {
+  it('should delete a transaction from db', async () => {
+    const user = await prisma.user.create({
+      data: fakeUser,
+    });
+    await prisma.transaction.create({
+      data: { ...transaction, user_id: user.id },
+    });
+
+    const sut = new PostgresDeleteTransactionRepository();
+
+    const result = await sut.execute(transaction.id);
+
+    expect(result).not.toBeNull();
+    expect(result.name).toBe(transaction.name);
+    expect(result.user_id).toBe(user.id);
+    expect(String(result.amount)).toBe(String(transaction.amount));
+    expect(result.type).toBe(transaction.type);
+    expect(dayjs(result.date).daysInMonth()).toBe(
+      dayjs(transaction.date).daysInMonth()
+    );
+    expect(dayjs(result.date).month()).toBe(dayjs(transaction.date).month());
+    expect(dayjs(result.date).year()).toBe(dayjs(transaction.date).year());
+  });
+
+  it('should call with correct values', async () => {
+    const sut = new PostgresDeleteTransactionRepository();
+    const prismaSpy = jest.spyOn(prisma.transaction, 'delete');
+
+    await sut.execute(transaction.id);
+
+    expect(prismaSpy).toHaveBeenCalledWith({
+      where: { id: transaction.id },
+    });
+  });
+});
