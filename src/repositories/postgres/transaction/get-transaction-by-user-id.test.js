74426865219e482a4bdc26257faac05ebc577db5
Vinicyos Ferreira <vinifr987@gmail.com>
2025-12-26 23:22:46 -0300

    test: ensure thorough test in PostgresGetTransactionByUserIdRepository

---
+++ src/repositories/postgres/transaction/get-transaction-by-user-id.test.js
@@ -0,0 +1,42 @@
+import { PostgresGetTransactionByUserIdRepository } from './get-transaction-by-user-id.js';
+import { user, transaction } from '../../../tests/index.js';
+import { prisma } from '../../../../prisma/prisma.js';
+import dayjs from 'dayjs';
+
+describe('Get Transaction By User Id Repository', () => {
+  it('should get transactions by user id on db', async () => {
+    await prisma.user.create({ data: user });
+    await prisma.transaction.create({
+      data: {
+        ...transaction,
+        user_id: user.id,
+      },
+    });
+
+    const sut = new PostgresGetTransactionByUserIdRepository();
+
+    const result = await sut.execute(user.id);
+
+    expect(result.length).toBe(1);
+    expect(result[0].name).toBe(transaction.name);
+    expect(result[0].type).toBe(transaction.type);
+    expect(result[0].user_id).toBe(user.id);
+    expect(String(result[0].amount)).toBe(String(transaction.amount));
+    expect(dayjs(result[0].date).daysInMonth()).toBe(
+      dayjs(transaction.date).daysInMonth()
+    );
+    expect(dayjs(result[0].date).month()).toBe(dayjs(transaction.date).month());
+    expect(dayjs(result[0].date).year()).toBe(dayjs(transaction.date).year());
+  });
+
+  it('should call prisma.transaction.findMany with correct values', async () => {
+    const sut = new PostgresGetTransactionByUserIdRepository();
+    const prismaSpy = jest.spyOn(prisma.transaction, 'findMany');
+    await sut.execute(user.id);
+    expect(prismaSpy).toHaveBeenCalledWith({
+      where: {
+        user_id: user.id,
+      },
+    });
+  });
+});
