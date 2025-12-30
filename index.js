202a4064f9df188c12b3493a95b39c48e734f63f
Vinicyos Ferreira <vinifr987@gmail.com>
2025-12-30 13:06:28 -0300

    refactor: create file to call API routes and middlewares

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 import 'dotenv/config';
-import express from 'express';
 import process from 'process';
-import { usersRouter, transactionsRouter } from './src/routes/index.js';
+import { app } from './src/app.js';
 
-const app = express();
-app.use(express.json());
 const port = process.env.PORT;
-
-app.use('/api/users', usersRouter);
-app.use('/api/transactions', transactionsRouter);
-
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
