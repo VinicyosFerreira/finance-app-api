5c5afcce2d45e55dc1e9e477628d43f29a9bf66b
Vinicyos Ferreira <vinifr987@gmail.com>
2025-12-30 18:49:15 -0300

    test: ensure complete testing of User Routes E2E

---
+++ src/routes/users.e2e.test.js
@@ -0,0 +1,155 @@
+import request from 'supertest';
+import { app } from '../app.js';
+import { user } from '../tests/index.js';
+import { faker } from '@faker-js/faker';
+import { TransactionType } from '../generated/prisma/client.js';
+
+describe('User Routes E2E Tests', () => {
+  it('POST /api/users return 201 and created user', async () => {
+    const response = await request(app)
+      .post('/api/users')
+      .send({
+        ...user,
+
+        id: undefined,
+      });
+
+    expect(response.status).toBe(201);
+  });
+
+  it('GET /api/users/:userId return 200 and user', async () => {
+    const { body: createdUser } = await request(app)
+      .post('/api/users')
+      .send({
+        ...user,
+        id: undefined,
+      });
+
+    const response = await request(app).get(`/api/users/${createdUser.id}`);
+
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual(createdUser);
+  });
+
+  it('PATCH /api/users/:userId return 200 and updated user', async () => {
+    const { body: createdUser } = await request(app)
+      .post('/api/users')
+      .send({
+        ...user,
+        id: undefined,
+      });
+
+    const updateUserPayload = {
+      first_name: faker.person.firstName(),
+      last_name: faker.person.lastName(),
+      email: faker.internet.email(),
+      password: faker.internet.password({
+        length: 7,
+      }),
+    };
+
+    const { body: updatedUser, status } = await request(app)
+      .patch(`/api/users/${createdUser.id}`)
+      .send(updateUserPayload);
+
+    expect(status).toBe(200);
+    expect(updatedUser.first_name).toBe(updateUserPayload.first_name);
+    expect(updatedUser.last_name).toBe(updateUserPayload.last_name);
+    expect(updatedUser.email).toBe(updateUserPayload.email);
+    expect(updatedUser.password).not.toBe(updateUserPayload.password);
+  });
+
+  it('DELETE /api/users/:userID return 200 and deleted user', async () => {
+    const { body: createdUser } = await request(app)
+      .post('/api/users')
+      .send({
+        ...user,
+        id: undefined,
+      });
+
+    const response = await request(app).delete(`/api/users/${createdUser.id}`);
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual(createdUser);
+  });
+
+  it('GET api/users/:userId/balance return 200 and user balance', async () => {
+    const { body: createdUser } = await request(app)
+      .post('/api/users')
+      .send({
+        ...user,
+        id: undefined,
+      });
+
+    await Promise.all([
+      request(app).post('/api/transactions').send({
+        user_id: createdUser.id,
+        name: faker.commerce.productName(),
+        date: faker.date.anytime().toISOString(),
+        type: TransactionType.EARNING,
+        amount: 10000,
+      }),
+      request(app).post('/api/transactions').send({
+        user_id: createdUser.id,
+        name: faker.commerce.productName(),
+        date: faker.date.anytime().toISOString(),
+        type: TransactionType.EXPENSE,
+        amount: 3000,
+      }),
+      request(app).post('/api/transactions').send({
+        user_id: createdUser.id,
+        name: faker.commerce.productName(),
+        date: faker.date.anytime().toISOString(),
+        type: TransactionType.INVESTMENT,
+        amount: 4000,
+      }),
+    ]);
+
+    const response = await request(app).get(
+      `/api/users/${createdUser.id}/balance`
+    );
+
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({
+      earnings: '10000',
+      expenses: '3000',
+      investments: '4000',
+      balance: '3000',
+    });
+  });
+
+  it('GET api/users/:userId return 404 when user not found', async () => {
+    const response = await request(app).get(
+      `/api/users/${faker.string.uuid()}`
+    );
+    expect(response.status).toBe(404);
+  });
+
+  it('GET /api/users/:userId/balance return 404 when user not found', async () => {
+    const response = await request(app).get(
+      `/api/users/${faker.string.uuid()}/balance`
+    );
+    expect(response.status).toBe(404);
+  });
+
+  it('PATCH /api/users/:userId return 404 when user not found', async () => {
+    const response = await request(app)
+      .patch(`/api/users/${faker.string.uuid()}`)
+      .send({
+        first_name: faker.person.firstName(),
+        last_name: faker.person.lastName(),
+        email: faker.internet.email(),
+        password: faker.internet.password({
+          length: 7,
+        }),
+      });
+
+    expect(response.status).toBe(404);
+  });
+
+  it('DELETE /api/users/:userID return 404 when user not found', async () => {
+    const response = await request(app).delete(
+      `/api/users/${faker.string.uuid()}`
+    );
+    expect(response.status).toBe(404);
+  });
+});
