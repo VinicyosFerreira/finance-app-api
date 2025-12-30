a24b0e0d3fd3a24ec709d1e220b42afa1e10bfa3
Vinicyos Ferreira <vinifr987@gmail.com>
2025-12-30 16:27:21 -0300

    feat: ensure Postgres UpdateUserRepository returns 404 if UserNotFoundError is thrown

--- src/repositories/postgres/user/update-user.js
+++ src/repositories/postgres/user/update-user.js
@@ -1,12 +1,24 @@
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/client';
+import { UserNotFoundError } from '../../../errors/user.js';
 import { prisma } from '../../../../prisma/prisma.js';
 
 export class PostgresUpdateUserRepository {
   async execute(userId, updateUserParams) {
-    return await prisma.user.update({
-      where: {
-        id: userId,
-      },
-      data: updateUserParams,
-    });
+    try {
+      return await prisma.user.update({
+        where: {
+          id: userId,
+        },
+        data: updateUserParams,
+      });
+    } catch (error) {
+      console.log(error);
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2025') {
+          throw new UserNotFoundError(userId);
+        }
+      }
+      throw error;
+    }
   }
 }
